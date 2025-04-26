b4a167f28152f68f2211dc454a719eb2c65d26de
Daniel Angulo Sanchez <dangulosanchez@gmail.com>
2025-04-26 16:01:20 -0400

    Initialize TypeScript environment
    Configure necessary .json to get npm environment to run on Mac. Temporarily comment out MongoDB functionality.

--- backend/index.ts
+++ backend/index.ts
@@ -14,11 +14,16 @@ app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 app.use(cors());
 app.use("/api/user", userRouter);
 
-const PORT: string | number = process.env.PORT || 5000;
+const PORT: string | number = process.env.PORT || 5001;
+console.log({...process.env})
 
-mongoose
-  .connect(process.env.MONGODB_URL || '')
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server Started On Port ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// mongoose
+//   .connect(process.env.MONGODB_URL || '')
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`Server Started On Port ${PORT}`))
+//   )
+//   .catch((error) => console.log(error.message));
